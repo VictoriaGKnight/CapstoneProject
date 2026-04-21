449e07ccbf8f05d1bd2b7ddaabbd9a7557ef84e0
VictoriaGKnight <vgknight1589@gmail.com>
2026-04-20 22:16:45 -0400

    fix vercel spa routing

--- CapstoneProject/src/main.jsx
+++ CapstoneProject/src/main.jsx
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -18,6 +19,25 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <App />
         </DataProvider>
       </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+
+import "./styles/theme.css";
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/components.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
