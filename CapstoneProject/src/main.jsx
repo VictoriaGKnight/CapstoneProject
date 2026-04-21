54b53c935a22c0b16da339e449f7b9788d221357
VictoriaGKnight <vgknight1589@gmail.com>
2026-04-20 22:20:56 -0400

    fix app render and vercel spa routing

--- CapstoneProject/src/main.jsx
+++ CapstoneProject/src/main.jsx
@@ -1,13 +1,13 @@
-/*
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App.jsx";
+
 import "./styles/theme.css";
 import "./styles/base.css";
 import "./styles/layout.css";
 import "./styles/components.css";
+
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { DataProvider } from "./context/DataContext.jsx";
 
@@ -19,25 +19,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <App />
         </DataProvider>
       </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-*/
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-
-import "./styles/theme.css";
-import "./styles/base.css";
-import "./styles/layout.css";
-import "./styles/components.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
